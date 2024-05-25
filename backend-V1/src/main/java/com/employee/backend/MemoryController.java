bc1f2e1289c6625a57ff930f8085a7f0d7ff2f74
gandhipratik65 <117957432+gandhipratik65@users.noreply.github.com>
2024-05-25 23:08:41 +0530

    Increase memory load

--- backend-V1/src/main/java/com/employee/backend/MemoryController.java
+++ backend-V1/src/main/java/com/employee/backend/MemoryController.java
@@ -10,14 +10,14 @@ public class MemoryController {
 
     @GetMapping("/stress")
     public String stressMemory() {
-        int arraySize = 10000000; // Size of the array to allocate
+        int arraySize = 1000000000; // Size of the array to allocate
         int[] memoryFillIntVar = new int[arraySize];
         for (int i = 0; i < arraySize; i++) {
             memoryFillIntVar[i] = i;
         }
         // Perform some operations to keep the CPU busy
         long start = System.currentTimeMillis();
-        while (System.currentTimeMillis() - start < 10000) { // 10 seconds
+        while (System.currentTimeMillis() - start < 10) { // 10 seconds
             for (int i = 0; i < arraySize; i++) {
                 memoryFillIntVar[i] = memoryFillIntVar[i] * 2;
             }
